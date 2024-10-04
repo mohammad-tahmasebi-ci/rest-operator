2fd06b6d98566d80bf102ed6413e3fe762f891af
Mohammad Tahmasebi <mohammad.tahmasebi@students.codeinstitute.net>
2024-10-04 14:34:36 +0000

    rest operator challenge

--- rest-parameter-syntax.js
+++ rest-parameter-syntax.js
@@ -3,8 +3,39 @@
  * command node rest-parameter-syntax.js in the terminal
  */
 
+/*
 // Regular function call 
+let sumAll = (a, b, c) => a + b + c;
+console.log(sumAll(1, 2, 3));
 
 // Extra arguments are ignored
+console.log(sumAll(1, 2, 3, 4, 5));
 
 // Function using ...rest
+let sumRest = (a, b, c, ...rest) => {
+    let sum = a + b + c;
+    for (let i of rest) {
+        sum += i;
+    }
+    return sum;
+}
+
+let total = sumRest(1, 2, 3, 4, 5, 6);
+console.log(total);
+*/
+
+let mixedLetters = ['b', 'd', 'a', 'c', 'f', 'e'];
+let moreMixedLetters = [...mixedLetters, 'h', 'k', 'g', 'j', 'i', 'l'];
+console.log(moreMixedLetters);
+const updateSortReverse = (arr, ...letters) => {
+    let result = [...arr];
+    for (let letter of letters) {
+        result.push(letter);
+    }
+    result.sort();
+    result.reverse();
+    return result;
+}
+
+let reverseSort = updateSortReverse(moreMixedLetters, 'n', 'm', 'o');
+console.log(reverseSort);
